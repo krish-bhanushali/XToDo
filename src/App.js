8c2846942a848f000d3a34b15041b333750c17e9
krish-bhanushali <bhanushali123krish@gmail.com>
2021-03-19 09:22:32 +0530

    v1.0 UI

---
+++ src/App.js
@@ -0,0 +1,13 @@
+
+
+import HomePage from './Components/home'
+
+function App() {
+  return (
+    <div className="App">
+     <HomePage />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/home.js
@@ -0,0 +1,57 @@
+import videoUrl from '../assets/backVideo1.mp4'
+import facebook from '../assets/facebook.png'
+import twitter from '../assets/twitter.png'
+import instagram from '../assets/instagram.png'
+import {useState }from 'react';
+
+const HomePage = () => {
+
+    const [toggle,setToggle] = useState(1)
+    const menuToggle = ['showcase active','showcase']
+    const toggleToggle = ['toggle active','toggle']
+     const doingToggle = () => {
+        if(toggle===0){
+            setToggle(1)
+        }else{
+            setToggle(0)
+        }
+       
+    }
+
+    return ( <div className="home-container">
+        <section className={menuToggle[toggle]}>
+            <header>
+                <h2 className="logo">XTODO</h2>
+                <div className={toggleToggle[toggle]} onClick={doingToggle}>
+
+                </div>
+                
+                
+            </header>
+            <video src={videoUrl} muted loop autoPlay></video>
+            <div className="overlay"></div>
+            <div className="text">
+                <h2>Hustle</h2>
+                <h3>List all your tasks</h3>
+                <p>Xtodo a secure blockchain based todo app</p>
+                <a href="/">Write It Up</a>
+            </div>
+            <ul className="social">
+                    <li><a href="/"><img src={facebook} alt=""/></a></li>
+                    <li><a href="/"><img src={instagram} alt=""/></a></li>
+                    <li><a href="/"><img src={twitter} alt=""/></a></li>
+            </ul>
+        </section>
+        <div className="menu">
+            <ul>
+                <li><a href="/">Home</a></li>
+                <li><a href="/">Account</a></li>
+                <li><a href="/">ToDos</a></li>
+                <li><a href="/">Motivation</a></li>
+                <li><a href="/">Contact</a></li>
+            </ul>
+        </div>
+    </div> );
+}
+ 
+export default HomePage;
